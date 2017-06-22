08ef4b839368c4179f89e51997ab767c3e3b11bc
Eric <zedonghe@gmail.com>
2017-06-22 12:09:44 +0800

    add

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float speed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 20.0f;
+	private Vector3 moveDirection = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController> ();
+		if (controller.isGrounded) {
+			Debug.Log ("Charactor on the ground");
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump"))
+				moveDirection.y = jumpSpeed;
+		} else {
+			Debug.Log ("Character is Not on the ground");
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}
